a456192b77a3acc309b13b793253ebf725127242
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-12-27 23:12:41 +0530

    Hosting issues

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage() {
+  return (
+    <main className="page">
+      <h1 className="title">Projects</h1>
+      {/* Add your projects content here */}
+    </main>
+  );
+}
